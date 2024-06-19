258092ca8ce919120e8ccade8b3c712250d2caf3
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-06-19 17:20:08 +0530

    Added component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { assets } from '../../assets/assets'
 function Navbar() {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <div className='leftbar'>
+      <img src={assets.logos} alt='' className='logo'/>
+      </div>
+      <div className="middlebar">
+        <ul className="navbar-menu">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Mobile App</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+      <div className="rightbar">
+        <img src={assets.search_icon} alt=''/>
+      <div className='navbar-search-icon'>
+        <img src={assets.basket_icon} alt=''/>
+      </div>
+      <div className="login">login</div>
+      </div>
     </div>
   )
 }
