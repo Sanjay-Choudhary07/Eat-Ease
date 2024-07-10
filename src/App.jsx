7bb6212e93f84f39d3b35e160a8e74d956a81cf9
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-07-10 21:19:57 +0530

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
-import {Routes} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/home/Home';
+import Cart from './pages/cart/Cart';
+import Placeorder from './pages/placeorder/Placeorder';
+// import './index.css'
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
       <Routes>
        <Route path ='/' element={<Home/>} />
+       <Route path ='/cart' element={<Cart/>} />
+       <Route path ='/order' element={<Placeorder/>} />
       </Routes>
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css';
 import Header from '../../components/header/Header';
+import Exploremenu from '../../components/exploremenu/Exploremenu';
 
 const Home = () => {
   return (
     <div>
       <Header/>     
+      <Exploremenu/>
     </div>
   )
 }
